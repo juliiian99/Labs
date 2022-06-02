d726079b02b5ea5dddfe59aa4e096b6262458402
Lourdes Grilli <luli.grilli.2002@gmail.com>
2022-06-02 17:24:48 -0300

    add2.1

---
+++ Unidad.2.Capitulo.1.Lab.1.Sintaxis/LabSintaxis2/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabSintaxis2
+{
+    class Program
+    {
+        public static object ToUpper { get; private set; }
+        public int Length { get; }
+
+        static void Main(string[] args)
+        { // con if anidados
+            Console.WriteLine("Ingresar un texto: ");
+
+            string inputTexto;
+            inputTexto = Console.ReadLine();
+            if (inputTexto != "")
+            {
+                Console.WriteLine("Ingrese opcion 1: MAYUSCULAS, 2: minusculas, 3: largo del texto");
+                ConsoleKeyInfo opcion = Console.ReadKey();
+                if (opcion.Key == ConsoleKey.D1)
+                    Console.WriteLine(inputTexto.ToUpper());
+                else if (opcion.Key == ConsoleKey.D2)
+                    Console.WriteLine(inputTexto.ToLower());
+                else if (opcion.Key == ConsoleKey.D3)
+                    Console.WriteLine(inputTexto.Length);
+                else Console.WriteLine("opcion no valida");
+            }
+            else Console.WriteLine("No ingreso un texto"); Console.ReadKey();
+            // con estructura case
+            {
+                Console.WriteLine("Ingresar un texto: ");
+
+                string inputText;
+                inputText = Console.ReadLine();
+                if (inputText != "")
+                {
+
+                    Console.WriteLine("Ingrese 1: MAYUSCULAS, 2: minusculas, 3: largo del texto");
+                    ConsoleKeyInfo opcion = Console.ReadKey();
+                    switch (opcion.Key)
+                    {
+                        case ConsoleKey.D1:
+                            Console.WriteLine(inputText.ToUpper());
+                            break;
+                        case ConsoleKey.D2:
+                            Console.WriteLine(inputText.ToLower());
+                            break;
+                        case ConsoleKey.D3:
+                            Console.WriteLine(inputText.Length);
+                            break;
+                        default:
+                            Console.WriteLine("No ingreso texto");
+                            break;
+                    }
+                    Console.ReadKey();
+                }
+                else Console.WriteLine("No ingreso texto"); Console.ReadKey();
+            }
+
+        }
+
+
+    }
+
+}
+
---
+++ Unidad.2.Capitulo.1.Lab.1.Sintaxis/LabSintaxis3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabSintaxis3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arreglo;
+            arreglo = new string[5];
+            int cantIteraciones = 5;
+            for (int i = 0; i < cantIteraciones; i++)
+            {
+                Console.WriteLine("Ingrese "+(i+1)+" string: ");
+                arreglo[i] = Console.ReadLine();
+            }
+            for (int i = 4; i >= 0; i--)
+            {
+                Console.WriteLine(arreglo[i]);
+                Console.WriteLine();
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
