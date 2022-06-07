2e400122e9a360fadb36a78e8980f46d93c9a7ff
juliiian99 <juliiianvilloria99@gmail.com>
2022-06-07 11:48:13 -0300

    Agrego lab4.1

---
+++ Unidad.4.Lab.1.ArchivosTXTyXML/Lab4.1/Lab4.1/Program.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Xml;
+
+namespace Lab4._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //FileStream lector = new FileStream("agenda.txt", FileMode.Open, FileAccess.Read, FileShare.Read);
+            //while (lector.Length > lector.Position)
+            //{
+            //    Console.Write((char)lector.ReadByte());
+            //}
+            //lector.Close();
+            //Console.ReadKey();
+
+            //StreamReader lector = File.OpenText("agenda.txt");
+            //string linea;
+            //do
+            //{
+            //    linea = lector.ReadLine();
+            //    if (linea != null)
+            //    {
+            //        Console.WriteLine(linea);
+            //    }
+            //} while (linea != null);
+            //lector.Close();
+            //Console.ReadKey();
+
+            Leer();
+            Escribir();
+            Console.ReadKey();
+
+            Console.WriteLine("Presione una tecla para generar el archivo agendaxml.xml con los datos de agenda.txt");
+            Console.ReadKey();
+            EscribirXML();
+            Console.WriteLine("Archivo agendaxml.xml generado correctamente\n\nPresione una tecla para ver su contenido");
+            Console.ReadKey();
+            Console.WriteLine();
+            LeerXML();
+            Console.ReadKey();
+        }
+
+        private static void Leer()
+        {
+            StreamReader lector = File.OpenText("agenda.txt");
+            string linea;
+            Console.WriteLine("Nombre\tApellido\te-mail\t\t\tTelefono");
+            do
+            {
+                linea = lector.ReadLine();
+                if (linea != null)
+                {
+                    string[] valores = linea.Split(';');
+                    Console.WriteLine("{0}\t{1}\t{2}\t{3}", valores[0], valores[1], valores[2], valores[3]);
+                }
+            } while (linea != null);
+            lector.Close();
+        }
+
+        public static void Escribir()
+        {
+            StreamWriter escritor = File.AppendText("agenda.txt");
+            Console.WriteLine("Ingrese nuevo contacto");
+            string rta = "5";
+            while (rta == "5")
+            {
+                Console.Write("Ingrese nombre: ");
+                string nombre = Console.ReadLine();
+                Console.Write("Ingrese apellido: ");
+                string apellido = Console.ReadLine();
+                Console.Write("Ingrese e-mail: ");
+                string email = Console.ReadLine();
+                Console.Write("Ingrese telefono: ");
+                string telefono = Console.ReadLine();
+                Console.WriteLine();
+                Console.WriteLine();
+
+                escritor.WriteLine(nombre + ";" + apellido + ";" + email + ";" + telefono);
+
+                Console.WriteLine("¿Desea ingresar otro contacto? (S/N)");
+                rta = Console.ReadLine();
+            }
+            escritor.Close();
+        }
+
+        public static void EscribirXML()
+        {
+            XmlTextWriter escritorXML = new XmlTextWriter("agendaxml.xml", null);
+            escritorXML.Formatting = Formatting.Indented;
+            escritorXML.WriteStartDocument(true);
+            escritorXML.WriteStartElement("DocumentElement");
+            StreamReader lector = File.OpenText("agenda.txt");
+            string linea;
+            do
+            {
+                linea = lector.ReadLine();
+                if (linea != null)
+                {
+                    string[] valores = linea.Split(';');
+                    escritorXML.WriteStartElement("contactos");
+                    escritorXML.WriteStartElement("nombre");
+                    escritorXML.WriteValue(valores[0]);
+                    escritorXML.WriteEndElement();
+                    escritorXML.WriteStartElement("apellido");
+                    escritorXML.WriteValue(valores[1]);
+                    escritorXML.WriteEndElement();
+                    escritorXML.WriteStartElement("email");
+                    escritorXML.WriteValue(valores[2]);
+                    escritorXML.WriteEndElement();
+                    escritorXML.WriteStartElement("telefono");
+                    escritorXML.WriteValue(valores[3]);
+                    escritorXML.WriteEndElement();
+                    escritorXML.WriteEndElement();
+                }
+            } while (linea != null);
+            escritorXML.WriteEndElement();
+            escritorXML.WriteEndDocument();
+            escritorXML.Close();
+        }
+
+        public static void LeerXML()
+        {
+            XmlTextReader lectorXML = new XmlTextReader("agendaxml.xml");
+
+            string tagAnterior = "";
+            while (lectorXML.Read())
+            {
+                if (lectorXML.NodeType == XmlNodeType.Element)
+                {
+                    tagAnterior = lectorXML.Name;
+                }else if (lectorXML.NodeType == XmlNodeType.Text)
+                {
+                    Console.WriteLine(tagAnterior + ": " + lectorXML.Value);
+                }
+            }
+            lectorXML.Close();
+        }
+    }
+}
